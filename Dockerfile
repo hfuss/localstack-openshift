7bf164810db02a1cafe0e5a52c03a09c13f61d60
Alan Bevier <awbevier@gmail.com>
2020-04-30 00:04:48 -0400

    clean up and set a tag

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM localstack/localstack:latest
+FROM localstack/localstack:0.10.8
 
 RUN chgrp -R 0 /etc/passwd && \
-    chmod -R g+rwX /etc/passwd
+    chmod -R g+rwX /etc/passwd && \
+    rm -rf /tmp/localstack/*
+
 COPY custom-entrypoint.sh /custom-entrypoint.sh
 ENTRYPOINT ["/custom-entrypoint.sh"]
 
--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# In OpenShift, containers are run as a random high number uid
-# that doesn't exist in /etc/passwd, but Ansible module utils
-# require a named user. So if we're in OpenShift, we need to make
-# one before Ansible runs.
-if [ `id -u` -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
+# In OpenShift, containers are run as a random high number uid that
+# doesn't exist in /etc/passwd, but Localstack requires a user named
+# localstack. So if we're in OpenShift, we need to make one before
+# localstack runs.
+if [ "$(id -u)" -ge 500 ] || [ -z "${CURRENT_UID}" ]; then
 
   cat << EOF > /tmp/passwd
 root:x:0:0:root:/root:/bin/bash
-localstack:x:`id -u`:`id -g`:,,,:/localstack:/bin/bash
+localstack:x:$(id -u):$(id -g):,,,:/localstack:/bin/bash
 EOF
 
   cat /tmp/passwd > /etc/passwd
